3f0770ce71dc1ba99d04af2383b7c58d35ecaf77
Jan Domanski <jandom@gmail.com>
2017-04-14 22:35:03 +0200

    Testing for a simple foo-bar card addition

--- README.md
+++ README.md
@@ -6,6 +6,11 @@ This will get you lots of points on Duolingo, and in turn will make you Very Coo
 Make sure you have selenium installed
 `pip install selenium`
 
+Install chromedriver to use on MacOS with chrome
+`brew install chromedriver`
+or geckodriver to use with FireFox
+`brew install geckodriver`
+
 Edit duolingo.py with your username and password.
 
 Change translations.py to whichever language you'd like. German by default :D
--- duolingo.py
+++ duolingo.py
@@ -7,75 +7,54 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from translations import translations
+import os, sys
 
 #setup
 
-fp = webdriver.FirefoxProfile()
+# fp = webdriver.FirefoxProfile()
+# fp.set_preference("webdriver.load.strategy", "unstable");
+# driver = webdriver.Firefox(firefox_profile=fp)
 
-fp.set_preference("webdriver.load.strategy", "unstable");
+chromedriver = "/usr/local/bin/chromedriver"
+os.environ["webdriver.chrome.driver"] = chromedriver
+driver = webdriver.Chrome(chromedriver)
 
-driver = webdriver.Firefox(firefox_profile=fp)
+#driver.maximize_window()
 
-driver.maximize_window()
 
 #login
-driver.get("https://www.duolingo.com")
-driver.find_element_by_id("sign-in-btn").click()
-driver.find_element_by_name("login").send_keys("USER")
-driver.find_element_by_name("password").send_keys("PASS",Keys.RETURN)
-WebDriverWait(driver, 10, 0.05).until(EC.presence_of_element_located((By.XPATH, "//a[@href='/skill/de/Basics-1']")))
-driver.get("https://www.duolingo.com/skill/de/Basics-1/practice")
-while True:
+driver.get("https://tinycards.duolingo.com")
+driver.find_element_by_class_name("_2A2uR").click()
+driver.find_element_by_class_name("_2a16Y").send_keys(username)
+driver.find_element_by_class_name("_3FjlE").send_keys(password,Keys.RETURN)
+WebDriverWait(driver, 10, 0.05).until(EC.presence_of_element_located((By.XPATH, "//a[@href='/profile']")))
+
+driver.get(url)
+
+try:
+	WebDriverWait(driver, 10, 0.05).until(EC.presence_of_element_located((By.CLASS_NAME, "_3rQ1C")))
+	driver.find_element_by_class_name("_3rQ1C").click()
+
+	cards = driver.find_elements_by_css_selector("._2qbTM")
+	print(len(cards))
+	for card in cards[-1:]:
+		left, right = card.find_elements_by_css_selector("textarea")
+		left.send_keys("foo")
+		right.send_keys("bar")
+
+ 	
+
+
+except Exception as e:
+	print e
+	driver.get(url)
 	try:
-		WebDriverWait(driver, 10, 0.05).until(EC.presence_of_element_located((By.ID, "start-button")))
-		driver.find_element_by_id("start-button").click()
-		for i in xrange(24):
-			try:
-			  WebDriverWait(driver, 3, 0.05).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='text-input' or @id='word-input' or @name='mediumSelect' or @id='sentence-0']")))
-			except TimeoutException as e:
-				pass
-				#ActionChains(driver).send_keys("DEUTSCHLAND",Keys.RETURN).perform()
-			if len(driver.find_elements_by_css_selector("div.text-to-translate")) > 0:
-				line = driver.find_element_by_css_selector("div.text-to-translate").text.encode( "utf-8" ).replace('\n', ' ').replace('\r', '')
-				try:
-				  translation = translations[line]
-				  ActionChains(driver).send_keys(translation).perform()
-				except KeyError as e:
-					with open("dictionary.py", "a") as myfile:
-					  myfile.write("\n"+'"'+line+'":"",')
-					print '"'+line+'":"",'
-			driver.find_elements_by_xpath("//input[@id='sentence-0']")
-			try:
-				driver.find_element_by_xpath("//input[@id='sentence-0']").click()
-			except:
-				pass
-			ActionChains(driver).send_keys(". 1",Keys.ARROW_DOWN,Keys.ARROW_LEFT,Keys.DELETE,Keys.RETURN).perform()
-			WebDriverWait(driver, 5, 0.05).until(EC.visibility_of_element_located((By.ID, "next_button")))
-			#WebDriverWait(driver, 5, 0.05).until(EC.invisibility_of_element_located((By.XPATH, "//*[@id='text-input' or @id='word-input']")))
-			driver.find_element_by_id("next_button").click()
-			#ActionChains(driver).send_keys(Keys.RETURN).perform()
-		#ActionChains(driver).send_keys(Keys.RETURN,Keys.RETURN).perform()
-		WebDriverWait(driver, 7, 0.05).until(EC.presence_of_element_located((By.XPATH, "//button[contains(text(),'Practice again')]")))
-		driver.find_element_by_xpath("//button[contains(text(),'Practice again')]").click()
-	except Exception as e:
-		#print e
-		driver.get("https://www.duolingo.com/skill/de/Basics-1/practice")
-		try:
-		    WebDriverWait(driver, 3).until(EC.alert_is_present(),
-		                                   'Timed out waiting for PA creation ' +
-		                                   'confirmation popup to appear.')
+	    WebDriverWait(driver, 3).until(EC.alert_is_present(),
+	                                   'Timed out waiting for PA creation ' +
+	                                   'confirmation popup to appear.')
 
-		    alert = driver.switch_to_alert()
-		    alert.accept()
-		    print "alert accepted"
-		except TimeoutException:
-		    print "no alert"
-#except TimeoutException as e:
-	#WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "skip_button")))
-	#driver.find_element_by_id("skip_button").click()
-	#WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.ID, "next_button")))
-	#driver.find_element_by_id("next_button").click()
-#
-#driver.quit()
-
-
+	    alert = driver.switch_to_alert()
+	    alert.accept()
+	    print "alert accepted"
+	except TimeoutException:
+	    print "no alert"
